bf95465d037512347f5eb19cdc68bc3ba38ae8d3
GFerA-lab <fer.ague.villa.02@gmail.com>
2025-08-28 17:35:28 -0300

    Add files via upload

--- RATON_LABERINTO_SLF/main.py
+++ RATON_LABERINTO_SLF/main.py
@@ -1,127 +1,73 @@
-#import random
+import copy
 
-"""""
-import time
-import os
-
-laberinto = [
-    list("##############"),
-    list("#    #      ##"), 
-    list("#  ### ####  #"),
-    list("#      ##### #"),
-    list("#   ## #     #"),
-    list("######## #####")
-]
-
-def verificar_colision(fila_personaje, col_personaje, fila_encontrar, col_encontrar):
-    if fila_personaje == fila_encontrar and col_personaje == col_encontrar:
-        return True
-    return False
-
-def calcular_distancia(tablero, fila_personaje, col_personaje, fila_encontrar, col_encontrar):    
-    #return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
-    filas = len(tablero)
-    columnas = len(tablero[0])
-    contador = 0
-    ban = False
-    while True:
-        
-        while True:
-            #derecha
-            if col_personaje + 1 < columnas and tablero[fila_personaje][col_personaje +1] != "#":
-                col_personaje += 1
-                contador += 1
-                if verificar_colision(fila_personaje, col_personaje, fila_encontrar, col_encontrar):
-                    break
-            else:
-                ban = True
-                break
-        
-        if ban: 
-            break
-        
-        while True:
-        #arriba
-            if fila_personaje - 1 >= 0 and tablero[fila_personaje-1][col_personaje] != "#":
-                fila_personaje -= 1
-                contador += 1
-                if verificar_colision(fila_personaje, col_personaje, fila_encontrar, col_encontrar):
-                    break
-            else:
-                ban = True
-                break
-        
-        if ban: 
-            break
-        
-        while True:
-            #izquierda
-            if col_personaje - 1 < columnas and tablero[fila_personaje][col_personaje-1] != "#":
-                col_personaje -=1
-                contador +=1
-                if verificar_colision(fila_personaje, col_personaje, fila_encontrar, col_encontrar):
-                    break
-            else:
-                ban = True
-                break
-        
-        if ban: 
-            break
-        
-        while True:
-        #abajo
-            if fila_personaje + 1 >= 0 and tablero[fila_personaje][col_personaje-1] != "#":
-                fila_personaje += 1
-                contador += 1
-                if verificar_colision(fila_personaje, col_personaje, fila_encontrar, col_encontrar):
-                    break
-            else:
-                ban = True
-                break
-            
-    return contador
-"""
-
-def buscar_objetivo(laberinto, x, y, x_obejtivo, y_objetivo, solucion, contador = 0):
+def buscar_objetivo(laberinto, x, y, x_obejtivo, y_objetivo, solucion, contador):
     # Verifica si hemos llegado a la salida
-    if x == len(laberinto) - 1 and y == len(laberinto[0]) - 1:
-        solucion[x_obejtivo][y_objetivo] = 1
+    if x == x_obejtivo and y == y_objetivo:
+        solucion[x][y] = 1
         contador += 1
         return True, contador
 
     # Verifica si la posición actual es válida
-    if 0 <= x < len(laberinto) and 0 <= y < len(laberinto[0]) and (laberinto[x][y] == 0 or solucion[x][y] == 0):
+    if 0 <= x < len(laberinto) and 0 <= y < len(laberinto[0]) and laberinto[x][y] == 0 and solucion[x][y] == 0:
         # Marca la posición como parte de la solución
         solucion[x][y] = 1
         contador += 1
 
-        # Mover hacia abajo
-        boolean, contador = buscar_objetivo(laberinto, x + 1, y, x_obejtivo, y_objetivo, solucion)
-        if boolean:
-            return True, contador
+        if y <= y_objetivo:
 
-        # Mover hacia la derecha
-        boolean, contador = buscar_objetivo(laberinto, x, y + 1, x_obejtivo, y_objetivo, solucion)
-        if boolean:
-            return True, contador
+             # Mover hacia abajo
+            boolean, contador = buscar_objetivo(laberinto, x + 1, y, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
 
-        # Mover hacia arriba
-        boolean, contador = buscar_objetivo(laberinto, x - 1, y, x_obejtivo, y_objetivo, solucion)
-        if boolean:
-            return True, contador
+            # Mover hacia la derecha
+            boolean, contador = buscar_objetivo(laberinto, x, y + 1, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
+            
+            # Mover hacia arriba
+            boolean, contador = buscar_objetivo(laberinto, x - 1, y, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
+            
+            # Mover hacia la izquierda
+            boolean, contador = buscar_objetivo(laberinto, x, y - 1, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
 
-        # Mover hacia la izquierda
-        boolean, contador = buscar_objetivo(laberinto, x, y - 1, x_obejtivo, y_objetivo, solucion)
-        if boolean:
-            return True, contador
+            # Si ninguna dirección funciona, retrocede
+            solucion[x][y] = 0
+            contador -= 1
+            return False, contador
+        
+        else:
+            
+            # Mover hacia arriba
+            boolean, contador = buscar_objetivo(laberinto, x - 1, y, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
+            
+            # Mover hacia la izquierda
+            boolean, contador = buscar_objetivo(laberinto, x, y - 1, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
 
-        # Si ninguna dirección funciona, retrocede
-        solucion[x][y] = 0
-        contador -= 1
-        return False, contador
+            # Mover hacia abajo
+            boolean, contador = buscar_objetivo(laberinto, x + 1, y, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
+
+            # Mover hacia la derecha
+            boolean, contador = buscar_objetivo(laberinto, x, y + 1, x_obejtivo, y_objetivo, solucion, contador)
+            if boolean:
+                return True, contador
+
+            # Si ninguna dirección funciona, retrocede
+            solucion[x][y] = 0
+            contador -= 1
+            return False, contador
 
     return False, contador
-
 
 """
 def buscar_objetivo(laberinto, x, y, solucion):
@@ -158,13 +104,13 @@ def get_valid_moves(tablero, fila, col):
     filas = len(tablero)
     columnas = len(tablero[0])
 
-    if fila + 1 < filas and tablero[fila+1][col] != "#":
+    if fila + 1 < filas and tablero[fila+1][col] != 1:
         moves.append((fila+1, col))
-    if fila - 1 >= 0 and tablero[fila-1][col] != "#":
+    if fila - 1 >= 0 and tablero[fila-1][col] != 1:
         moves.append((fila-1, col))
-    if col + 1 < columnas and tablero[fila][col+1] != "#":
+    if col + 1 < columnas and tablero[fila][col+1] != 1:
         moves.append((fila, col+1))
-    if col - 1 >= 0 and tablero[fila][col-1] != "#":
+    if col - 1 >= 0 and tablero[fila][col-1] != 1:
         moves.append((fila, col-1))
 
     return moves
@@ -172,7 +118,8 @@ def get_valid_moves(tablero, fila, col):
 def movimiento(laberinto, fila, col, pasos, gato, otro_fila, otro_col):
     if pasos == 0:
         solucion = [[0 for _ in range(len(laberinto[0]))] for _ in range(len(laberinto))]
-        boolean, contador = buscar_objetivo(laberinto, fila, col, otro_fila, otro_col, solucion)
+        contador = 0
+        boolean, contador = buscar_objetivo(laberinto, fila, col, otro_fila, otro_col, solucion, contador)
         return contador, None
 
     if gato:
@@ -259,30 +206,94 @@ if __name__ == "__main__":
 
 """
 def mostrar(laberinto):
-    print("\n".join("".join(fila) for fila in laberinto))
-    print()
+    simbolos = {0: "⬜", 1: "🟩", 2: "🐭", 3: "🐱", 4: "🚪", 5: "😿", 6:"😼"}
+    for i in range(len(laberinto)):
+        for j in range(len(laberinto[0])):
+            print(simbolos[laberinto[i][j]], end=" ")
+        print()
+    print("\n")
+
+def validar_direccion(tablero, direccion, fila_jugador, col_jugador):
+    
+    movimientos = {
+        "W": (-1, 0),  # arriba
+        "S": (1, 0),   # abajo
+        "A": (0, -1),  # izquierda
+        "D": (0, 1)    # derecha
+    }
+    
+    direccion = direccion.upper()
+    if direccion in movimientos:
+        x_aux, y_aux = movimientos[direccion]
+        moves = get_valid_moves(tablero, fila_jugador, col_jugador)
+
+        if (fila_jugador + x_aux, col_jugador + y_aux) in moves:
+            return True, (fila_jugador + x_aux, col_jugador + y_aux)
+    
+    return False, (fila_jugador, col_jugador)
 
 def main():
     laberinto = [
-        [0, 1, 0, 0, 1],
+        [0, 0, 0, 0, 0],
+        [1, 0, 1, 1, 0],
         [0, 0, 0, 0, 0],
         [0, 1, 0, 1, 0],
-        [0, 0, 0, 1, 0]
+        [1, 0, 0, 0, 0]
     ]
+
+    aux_laberinto = copy.deepcopy(laberinto)
 
     raton_fila, raton_col = 0, 0
     gato_fila, gato_columna = 3, 4
 
-    laberinto[raton_fila][raton_col] = "R"
-    laberinto[gato_fila][gato_columna] = "G"
+    laberinto[raton_fila][raton_col] = 2
+    laberinto[gato_fila][gato_columna] = 3
+
+    fila_salida = 4
+    col_salida = 4
+    laberinto[fila_salida][col_salida] = 4
 
     mostrar(laberinto)
 
-    # if buscar_objetivo(laberinto, 0, 0, solucion):
-    #     for fila in solucion:
-    #         print(fila)
-    # else:
-    #     print("No hay solución para el laberinto.")
+    while True :
+
+        direccion = input("Ingrese la direccion W/S/D/A: ")
+
+        if direccion.isalpha():
+
+            validacion, mov_raton = validar_direccion(aux_laberinto, direccion, raton_fila, raton_col)
+
+            if validacion:
+                laberinto[raton_fila][raton_col] = 0
+                raton_fila, raton_col = mov_raton
+                laberinto[raton_fila][raton_col] = 2
+            else:
+                print("NO puedes ir hacia allí")
+        else:
+            print("Ingrese una direccion valida")
+
+        if raton_fila == fila_salida and raton_col == col_salida:
+            print("El raton llego a la Salida")
+            laberinto[raton_fila][raton_col] = 4
+            laberinto[gato_fila][gato_columna] = 5
+            break
+        eval1, mov_gato = movimiento(aux_laberinto, gato_fila, gato_columna, 3, True, raton_fila, raton_col)
+
+        laberinto[gato_fila][gato_columna] = 0
+        laberinto[fila_salida][col_salida] = 4
+        gato_fila, gato_columna = mov_gato
+        laberinto[gato_fila][gato_columna] = 3
+
+        if gato_fila == raton_fila and gato_columna == raton_col:
+            laberinto[raton_fila][raton_col] = 0
+            laberinto[gato_fila][gato_columna] = 6
+            print("El gato atrapo al raton")
+            break
+
+        mostrar(laberinto)
+    
+    mostrar(laberinto)
+    
 
 if __name__ == "__main__":
     main()
